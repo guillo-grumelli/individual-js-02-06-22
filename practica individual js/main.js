2b1c18b864d61c41b75d54785714a9d60f84cd0f
guillo-grumelli <85719375+guillo-grumelli@users.noreply.github.com>
2022-06-02 09:58:19 -0300

    Add files via upload

---
+++ practica individual js/main.js
@@ -0,0 +1,26 @@
+//ejercicio 1
+
+//let alumnos=["Silena", " Franco", " Guillermina", " Julian", " Milena"]
+//document.write(alumnos)
+
+//ejercicio 2
+
+//let alumnos=["Silena", " Franco", " Guillermina", " Julian", " Milena"]
+//document.write(alumnos[4])
+
+//ejercico 3
+//let alumnos=["Silena", " Franco", " Guillermina", " Julian", " Milena"]
+//let indice= 2;
+//alumnos[indice]="Milton";
+//document.write(alumnos);
+
+//ejercicio 4
+let alumnos=["Silena", " Franco", " Milton", " Julian", " Milena"]
+alumnos.push("alberto")
+alert("se ha unido Alberto a la clase")
+
+
+
+
+
+
